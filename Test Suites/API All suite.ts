988930867e8cab611269b4f7400c4ec9315dfa28
krisg <krisg@DESKTOP-BANK8JO.hub>
2020-06-24 08:28:42 +1000

    First Commit

---
+++ Test Suites/API All suite.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>API All suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>09413886-fb3b-46c7-93f3-35af8c83ee94</testSuiteGuid>
+   <testCaseLink>
+      <guid>c11c94fd-67e2-4071-b237-31237251f05a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Test case/Delete Function</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>162fa395-ed6d-4709-84ff-3497a89b0d2c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Test case/Name check</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9d161984-d39a-4ac0-ae92-aaba671faba3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorChecks/Add2Number</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f8be5b86-d1e1-4f41-bf1b-d9d7f76ecdaf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Test case/Delete Function</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>61b2c9fb-c037-48f7-98d4-36c5cb39c2ca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Test case/Name check</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e7302044-4c7f-4c4b-86b8-e9f3db57dc50</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorChecks/Add2Number</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
